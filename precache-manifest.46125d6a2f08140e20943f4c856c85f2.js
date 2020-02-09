907b2497e41e4941c39202169693dbd15465c851
Arjun <arjungupta2110@gmail.com>
2020-02-09 22:28:16 +0000

    Updates

---
+++ precache-manifest.46125d6a2f08140e20943f4c856c85f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91eb668f9e75c4211fb01ba8c021e41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68117da0fc39cacb32df",
+    "url": "/static/css/2.86b88f3a.chunk.css"
+  },
+  {
+    "revision": "0233763d160782af0e00",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68117da0fc39cacb32df",
+    "url": "/static/js/2.fb61637a.chunk.js"
+  },
+  {
+    "revision": "a70d8924bd8a61bb6d269ab467d9824b",
+    "url": "/static/js/2.fb61637a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0233763d160782af0e00",
+    "url": "/static/js/main.79fd480d.chunk.js"
+  },
+  {
+    "revision": "2c7ff97c08a33fba0ba6",
+    "url": "/static/js/runtime-main.a0aed81d.js"
+  }
+]);
